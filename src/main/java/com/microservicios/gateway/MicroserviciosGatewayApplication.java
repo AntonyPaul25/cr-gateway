bd56fc726c06e3e9c1c0707967e0fb71cacc2f8f
Boris <boriscamayo@gmail.com>
2021-08-01 20:14:45 -0500

    cambios para azure AD

--- src/main/java/com/microservicios/gateway/MicroserviciosGatewayApplication.java
+++ src/main/java/com/microservicios/gateway/MicroserviciosGatewayApplication.java
@@ -3,7 +3,11 @@ package com.microservicios.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableEurekaClient
 @SpringBootApplication
 public class MicroserviciosGatewayApplication {
