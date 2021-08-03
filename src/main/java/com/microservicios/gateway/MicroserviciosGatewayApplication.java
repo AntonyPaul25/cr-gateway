238aba200666442d5505590f051f66a96f690570
Boris <boriscamayo@gmail.com>
2021-08-03 10:31:36 -0500

    disable oautgh2

--- src/main/java/com/microservicios/gateway/MicroserviciosGatewayApplication.java
+++ src/main/java/com/microservicios/gateway/MicroserviciosGatewayApplication.java
@@ -3,11 +3,9 @@ package com.microservicios.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableEurekaClient
 @SpringBootApplication
 public class MicroserviciosGatewayApplication {
